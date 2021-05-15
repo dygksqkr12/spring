5eddd5acb3c87068ca3e376f5220c29bd581ab08
yohan <pyh52894@gmail.com>
2021-05-15 22:46:34 +0900

    21.05.15 KH spring study

---
+++ spring-bean/src/main/java/com/kh/spring/tv/model/vo/SamsungTv.java
@@ -0,0 +1,30 @@
+package com.kh.spring.tv.model.vo;
+
+public class SamsungTv implements Tv {
+
+	private RemoteControl remocon;
+	
+	public void setRemocon(RemoteControl remocon) {
+		System.out.println("setRemocon : " + remocon);
+		this.remocon = remocon;
+	}
+	
+	public SamsungTv(RemoteControl remocon) {
+		System.out.println("SamsungTv객체생성 : " + remocon);
+		this.remocon = remocon;
+	}
+	
+	public SamsungTv() {
+		System.out.println("SamsungTv객체를 생성했습니다.");
+	}
+	
+	@Override
+	public void powerOn() {
+		System.out.println("SamsungTv의 전원을 켰습니다.");
+	}
+
+	public void changeChannel(int no) {
+		this.remocon.changeChannel(no);
+	}
+
+}
